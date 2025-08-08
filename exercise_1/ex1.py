64d6774c630b76277bb2648a5827b93ebacf7d95
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 10:49:40 +0700

    Update ex1.py

--- exercise_1/ex1.py
+++ exercise_1/ex1.py
@@ -7,3 +7,20 @@ print ("I'd much rather you 'not'.")
 print ('I "said" do not touch this.')
 
 #This is printing command, "To start something big, try from small things first"
+
+#STUDY DRILLS
+#SD-1: Make your script print another line.
+print ("There is first time for everything")
+
+#SD-2: Make your script print only one of the lines.
+print ("Hello World")
+#print ("Hello Again")
+#print ("I like typing this.")
+#print ("This is fun.")
+#print ('Yay! Printing.')
+#print ("I'd much rather you 'not'.")
+#print ('I "said" do not touch this.')
+#So i use the (#) symbol to make my script only print one of the lines
+
+#SD-3: Put a # (octothorpe) character at the beginning of a line. What did it do? Try to find out what this character does
+# Octothorpe symbol is used to tell the python to ignore all script after that symbol. It can be handy to make some comment or make a notice about the coding processes
