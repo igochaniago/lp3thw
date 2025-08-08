b372fd69b688d08447414786e90129ae8801412d
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 10:55:39 +0700

    Create ex2.py

---
+++ exercise_2/ex2.py
@@ -0,0 +1,23 @@
+# A comment, this is so you can read your program later.
+# Anything after the # is ignored by python.
+
+print ("I could have code like this.") # and the comment after is ignored
+
+# You can also use a comment to "disable" or comment out code:
+# print ("This won't run.")
+
+print ("This will run.")
+
+
+#STUDY DRILLS
+#SD-1:Find out if you were right about what the # character does and make sure you know what it’s called (octothorpe or pound character).
+#Yes i was right, i called it as tanda pagar. The symbol is to tell python that every line after # symbol is to be ignored.
+
+#SD-2: Take your ex2.py file and review each line going backward. Start at the last line, and check each word in reverse against what you should have typed.
+#Okay
+
+#SD-3: Did you find more mistakes? Fix them.
+#No, i think ther are no mistake anymore
+
+#SD-4: Read what you typed above out loud, including saying each character by its name. Did you find more mistakes? Fix them.
+#Okay
