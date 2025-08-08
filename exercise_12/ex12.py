6f3934a147de6974cdf1385782ef63528cc56328
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 14:43:27 +0700

    Create ex12.py

---
+++ exercise_12/ex12.py
@@ -0,0 +1,18 @@
+age = input ("How old are you? ")
+height = input ("How tall are you? ")
+weight = input ("How much do you weigh? ")
+
+print (f"So, you're {age} old, {height} tall and {weight} heavy.")
+
+#STUDY DRILLS
+#SD-1. In Terminal, where you normally run python3.6 to run your scripts, type pydoc input. Read what it says. If you’re on Windows try python3.6 -m pydoc input instead.
+#Done
+
+#SD-2. Get out of pydoc by typing q to quit.
+#Done
+
+#SD-3. Look online for what the pydoc command does.
+#Done
+
+#SD-4. Use pydoc to also read about open, file, os, and sys. It’s alright if you do not understand those; just read through and take notes about interesting thing
+#Done
