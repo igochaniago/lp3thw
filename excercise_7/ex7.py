a01f410f69de88e98a6514c10f6808c630807ae1
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 13:51:36 +0700

    Create ex7.py

---
+++ excercise_7/ex7.py
@@ -0,0 +1,39 @@
+print ("Mary had a little lamb.") #Print string
+print ("Its fleece was white as {}.".format('snow')) #Print string
+print ("And everywhere that Mary went.") #Print string
+print ("." * 10) #it is multiply the dots by 10 times
+
+end1 = "C" #Defining variable end1
+end2 = "h" #Defining variable end2
+end3 = "e" #Defining variable end3
+end4 = "e" #Defining variable end4
+end5 = "s" #Defining variable end5
+end6 = "e" #Defining variable end6
+end7 = "B" #Defining variable end7
+end8 = "u" #Defining variable end8
+end9 = "r" #Defining variable end9
+end10 = "g" #Defining variable end10
+end11 = "e" #Defining variable end11
+end12 = "r" #Defining variable end12
+
+#watch end = ' ' at the end. try removing it to see what happens
+print (end1 + end2 + end3 + end4 + end5 + end6, end = ' ') #end = ' ' is used to make line 21 is at the same line as line 20 with space between line 20 and 21
+print (end7 + end8 + end9 + end10 + end11 + end12) #Continuation of line 20
+
+#output is Cheese Burger
+
+#STUDY DRILLS
+#SD-1. Go back through and write a comment on what each line does.
+#Done
+
+#SD-2. Read each one backward or out loud to find your errors.
+#Done
+
+#SD-3. From now on, when you make mistakes, write down on a piece of paper what kind of mistake you made.
+#Okay
+
+#SD-4. When you go to the next exercise, look at the mistakes you have made and try not to make them in this new one.
+#Okay
+
+#SD-5. Remember that everyone makes mistakes. Programmers are like magicians who fool everyone into thinking they are perfect and never wrong, but it’s all an act. They make mistakes all the time.
+#Okay
