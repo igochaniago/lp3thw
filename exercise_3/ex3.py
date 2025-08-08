d7046201089b81703d479257b0a6d8456c86f846
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 11:08:51 +0700

    Create ex3.py

---
+++ exercise_3/ex3.py
@@ -0,0 +1,43 @@
+print ("I will now count my chickens:")
+
+#Conducting addition(+) and division(/) of hens
+print ("Hens", 25 + 30 / 6)
+#Conducting substraction(-), multiplication(*), and modulus(%) of roosters
+print ("Roosters", 100 - 25 * 3 % 4)
+
+print ("Now I will count the eggs:")
+
+#Conducting addition(+), susbtraction(-), modulus(%), and division(/) of eggs
+print (3 + 2 + 1 - 5 + 4 % 2 - 1 / 4 + 6)
+
+print ("Is it true that 3 + 2 < 5 - 7?")
+
+#Conducting a comparison and using some boolean True or False to know the answer
+print (3 + 2 < 5 - 7)
+
+#Conducting addition(+) and substraction(-). But, with the answer next to it. We can look here that the operation is after comma and does not inside the string("...")
+print ("What is 3 + 2?", 3 + 2)
+print ("What is 5 - 7?", 5 - 7)
+
+print ("Oh, that's why it's False.")
+
+print ("How about some more.")
+
+#The same as line 16, but with the answer next to it.
+print ("Is it greater?", 5 > -2)
+print ("Is it greater or equal?", 5 >= -2)
+print ("Is it less or equal?", 5 <= -2)
+
+
+#STUDY DRILLS
+#SD-1: Above each line, use the # to write a comment to yourself explaining what the line does.
+#Done
+
+#SD-2: Remember in Exercise 0 when you started python3.6? Start python3.6 this way again and, using the math operators, use Python as a calculator.
+#Done
+
+#SD-3: Find something you need to calculate and write a new .py file that does it.
+#Refer to ex3_drill.py
+
+#SD-4: Rewrite ex3.py to use floating point numbers so it’s more accurate. 20.0 is floating point.
+#Refer to ex3_drill.py
