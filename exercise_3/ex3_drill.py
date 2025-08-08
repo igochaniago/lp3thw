28ef5ede80e0574dee3075ed0708cc6afab0262b
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 11:15:29 +0700

    Create ex3_drill.py

---
+++ exercise_3/ex3_drill.py
@@ -0,0 +1,17 @@
+#STUDY DRILL -> from ex3.py
+#3. Find something you need to calculate and write a new .py file that does it.
+
+#Ibu Sari pergi ke pasar untuk membeli bahan makanan. Ia membeli:
+#3 kg apel, harga per kg-nya Rp15.000
+#2 kg wortel, harga per kg-nya Rp10.000
+#5 bungkus tahu, harga per bungkus Rp3.000
+#Saat akan membayar, Ibu Sari menyadari bahwa ia hanya membawa uang Rp150.000. Penjual memberi diskon Rp10.000 untuk total belanjaan Ibu Sari.
+
+#1. Berapa total harga semua belanjaan sebelum diskon?
+print(3*15000 + 2*10000 + 5*3000)
+#2. Berapa total yang harus dibayar setelah diskon?
+print((3*15000 + 2*10000 + 5*3000)-10000)
+#3. Berapa sisa uang Ibu Sari setelah membayar?
+print(150000-((3*15000 + 2*10000 + 5*3000)-10000))
+#4. Jika belanjaan Ibu Sari akan dibagi rata ke dalam 5 kantong belanja, berapa rupiah nilai isi tiap kantong?
+print(((3*15000 + 2*10000 + 5*3000)-10000)/5)
