13551fa232763e47e57af2d21aa38de69cfeac0d
Igo Chaniago <65951124+igochaniago@users.noreply.github.com>
2025-08-08 14:03:55 +0700

    Create ex9.py

---
+++ exercise_9/ex9.py
@@ -0,0 +1,16 @@
+days = "Mon Tue Wed Thu Fri Sat Sun" #Defining 'days'
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug" #\n is for print in the new lines without getting new lines in coding process
+
+print ("Here are the days: ", days) #printing 'days'
+print ("Here are the months: ", months) #printing 'month'
+
+print ("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""") #Using triple quote (""") to print as it is 
+
+#STUDY DRILLS
+#Check your work, write down your mistakes, try not to make them on the next exercise. Are you breaking your code and then fixing it? In other words, repeat the Study Drills from Exercise 7.
+#Done
